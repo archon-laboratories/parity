ce4edd145ec8ec1883170085384a320691f86cd7
SAM <samvbeckmann@gmail.com>
2015-06-15 16:19:18 -0500

    initial commit

---
+++ src/com/samvbeckmann/parity/IAgent.java
@@ -0,0 +1,10 @@
+package com.samvbeckmann.parity;
+
+/**
+ * parity, class made on 6/15/2015.
+ *
+ * @auther Sam Beckmann
+ */
+public interface IAgent
+{
+}
---
+++ src/com/samvbeckmann/parity/Parity.java
@@ -0,0 +1,12 @@
+package com.samvbeckmann.parity;
+
+/**
+ * Created by SAM on 6/15/2015.
+ */
+public class Parity
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
