de39fe496ca2ff6207cd114cf0c572b988461718
SAM <samvbeckmann@gmail.com>
2015-07-02 14:33:52 -0500

    Set up initial gradle support.

--- src/main/java/com/samvbeckmann/parity/reference/Names.java
+++ src/main/java/com/samvbeckmann/parity/reference/Names.java
@@ -9,9 +9,9 @@ public final class Names
 {
     public final class FXMLPaths
     {
-        private static final String PACKAGE_PREFIX = "view/";
+        private static final String PACKAGE_PREFIX = "/fxml/";
 
-        public static final String ROOT_LAYOUT = PACKAGE_PREFIX + "rootLayout.fxml";
+        public static final String ROOT_LAYOUT = PACKAGE_PREFIX + "RootLayout.fxml";
         public static final String AGENT_VIEW = PACKAGE_PREFIX + "agentView.fxml";
         public static final String CONFIGURATION_VIEW = PACKAGE_PREFIX + "configurationView.fxml";
         public static final String AGENT_ADD_DIALOGUE = PACKAGE_PREFIX + "AgentAddDialogue.fxml";
